fb623c55653d777886a6c97b81357859b488076d
Ghanishtha <85752905+GHANISHTHA0511@users.noreply.github.com>
2022-06-05 11:23:21 +0530

    Update myportfolio.js

--- myportfolio.js
+++ myportfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front End Web Developer", "Graphic Designer", "Blogger"],
+        strings: ["Front End Web Developer", "Graphic Designer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front End Web Developer", "Graphic Designer", "Blogger"],
+        strings: ["Front End Web Developer", "Graphic Designer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
